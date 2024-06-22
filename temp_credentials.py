783eb69c55f377885fa8ce3b8db4f167cf8085b3
Elamparithi <70383944+Elam-parithi@users.noreply.github.com>
2024-06-23 02:42:01 +0530

    Update temp_credentials.py

--- temp_credentials.py
+++ temp_credentials.py
@@ -1,13 +1,8 @@
 
-# mine "AIzaSyDTWL7cVqvBC4lmGjr3RJUeC6CvYktcr6w"
-# "AIzaSyC0HkvL-_TXi_ba7jY_QYm9GPSpvxnAut0"
-
-api_key = "AIzaSyBl8fp2eq_aPnuz62rcBjfkJ2QwPYe_hc4"
-
-SQL_host = '192.168.0.114'
+SQL_host = 'localhost'
 SQL_port = 3307
 SQL_user = 'root'
 
-SQL_pswd = '1king#lanka'
+SQL_pswd = ''
 SQL_DB = 'Utube_DHW4'
 

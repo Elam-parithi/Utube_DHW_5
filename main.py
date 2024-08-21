4ded6e41decfd74d27fc44d7e87439c9aa04e25f
Elam-parithi <elamparithi17091999@gmail.com>
2024-08-21 21:32:38 +0530

    no need for main on streamlit cloud.

--- main.py
+++
@@ -1,8 +0,0 @@
-
-# This is my forth try on creating Utube_DHW.
-# I was not satisfied with previous creations.
-
-import os
-if __name__ == '__main__':
-    os.system('streamlit run Utube_website.py')
-

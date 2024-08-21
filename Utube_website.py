838f018168e3b33be42607664e9ff486cbc040d4
Elam-parithi <elamparithi17091999@gmail.com>
2024-08-21 21:49:51 +0530

    code snippet added for streamlit module issue.

--- Utube_website.py
+++ Utube_website.py
@@ -1,6 +1,25 @@
 # Utube_website.py
 
 # This is a streamlit website run this directly on command prompt using streamlit run Utube_website.py
+import os
+import subprocess
+import pkg_resources
+
+def install_if_missing(package_name, version=None):
+    try:
+        # Check if the package is already installed
+        dist = pkg_resources.get_distribution(package_name)
+        if version and dist.version != version:
+            raise pkg_resources.DistributionNotFound
+    except pkg_resources.DistributionNotFound:
+        # Package not found or version mismatch
+        print(f"Installing {package_name}...")
+        subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', f"{package_name}=={version}" if version else package_name])
+
+# Install required packages
+install_if_missing('google-api-core', '2.19.0')
+install_if_missing('google-api-python-client', '2.131.0')
+install_if_missing('google-auth', '2.29.0')
 
 from utube_DHW_aux_modules import *
 from streamlit_option_menu import option_menu

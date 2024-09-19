4f48024c28d8cc910c2463fd8bb474dca2317267
Elam-parithi <elamparithi17091999@gmail.com>
2024-09-19 21:17:36 +0530

    colour_pick and status update was moved to html_addon.py

--- config_page.py
+++ config_page.py
@@ -25,40 +25,6 @@ def status_banner(value_key, constructor):
         pass
 
 
-def colour_pick(constructor):
-    no_state = "orange"
-    valid_state = "lightgreen"
-    invalid_state = "red"  # "rgb(253, 109, 109)"
-    try:
-        colour_status = st.session_state[constructor].is_connected
-    except AttributeError:
-        return no_state
-    if colour_status is True:
-        output_colour = valid_state
-    elif colour_status is False:
-        output_colour = invalid_state
-    else:
-        output_colour = no_state
-    return output_colour
-
-
-def status_update():
-    api_color = colour_pick("youTube_API")
-    sql_color = colour_pick("MySQL_URL")
-    mdb_color = colour_pick("MongoDB_URI")
-    repeated = " width: 250px; height: 40px; display: flex; justify-content: center; align-items: center; border-radius: 8px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); font-size: 18px; text-align: center;"
-    html_code = f"""
-        <div id="status-container" style="display: flex; gap: 20px; border:50px; align-items: center; justify-content: center; border-radius: 8px; box-shadow: 0 4px 8px lightblue;
-        background-color: rgba(215, 218, 231, 0.918);">
-            <div><h3 style="color: black;"> Status: </h3></div> 
-            <div style="background-color: {api_color}; {repeated}"><h4>API Key</h4></div>
-            <div style="background-color: {sql_color}; {repeated}"><h4>SQL URL</h4></div>
-            <div style="background-color: {mdb_color}; {repeated}"><h4>Mongo URI</h4></div>
-        </div>
-        """
-    st.html(html_code)
-
-
 def repeater_entry(paragraphs, value_key, constructor: str):
     # "youTube_API", "MySQL_URL", "MongoDB_URI"
     st.text(paragraphs)

94374ebfba6728a648fc1d5f61a5c52c17f9a978
Elam-parithi <elamparithi17091999@gmail.com>
2024-11-17 13:29:30 +0530

    program to download NLTK data.

---
+++ Nltk_downloading.py
@@ -0,0 +1,12 @@
+"""
+pip install nltk
+pip install --upgrade nltk
+
+"""
+import nltk
+
+print(nltk.__version__)
+
+nltk.download('all')  # Download all NLTK data (optional)
+
+# FIXME: Download interrupted. Fix the internet and resume as soon as possible.

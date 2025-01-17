77fe950b49e79b8c1409a1b8be8d93bebea8a8ff
Elam-parithi <elamparithi17091999@gmail.com>
2025-01-17 23:24:32 +0530

    unwanted files

--- ploting_page.py
+++
@@ -1,9 +0,0 @@
-from config_and_auxiliary import *
-
-
-def analyze_page():
-    """
-        In this page instead of getting the data directly it will get the last 10 data from the MySQL DB
-        which is extracted from Home page and stored in the database mentioned above.
-    """
-    st.subheader("Analyze")

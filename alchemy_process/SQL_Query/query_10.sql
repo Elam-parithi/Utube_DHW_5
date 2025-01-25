dcbbe0cb451445684cc6ba151fe25298d43860a7
Elam-parithi <elamparithi17091999@gmail.com>
2025-01-26 00:54:54 +0530

    moved to a new folder

--- alchemy_process/SQL_Query/query_10.sql
+++
@@ -1,9 +0,0 @@
-
--- Question 10
-
-/*
-Which videos have the highest number of comments, and what are their corresponding channel names?
-*/
-
-SELECT channels.channel_name, videos.video_name, videos.comment_count AS Total_Comments FROM videos
-        JOIN channels ON channels.channel_id = videos.channel_id ORDER BY videos.comment_count DESC;
